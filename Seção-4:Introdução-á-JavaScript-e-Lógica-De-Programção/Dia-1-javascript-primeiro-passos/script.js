17df51a62b24eefaf47fb0b407039ecbf1df33c4
Guilherme <guiguisantosaraujo@gmail.com>
2022-09-21 18:32:30 -0300

    Finalizei o Primeiro exercício and Começando o Segundo exercício

---
+++ Seção-4:Introdução-á-JavaScript-e-Lógica-De-Programção/Dia-1-javascript-primeiro-passos/script.js
@@ -0,0 +1,39 @@
+
+
+let aad = 1;
+let bad = 1;
+let resultadoAdição = aad + bad;
+ console.log(resultadoAdição);
+ ///////////////////////////
+
+let asb = 2;
+let bsb = 1;
+let resultadoSubtração = asb - bsb;
+console.log(resultadoSubtração);
+////////////////////////////
+
+let amp = 2;
+let bmp = 2;
+let resultadoMultiplicação = amp * bmp;
+console.log(resultadoMultiplicação);
+///////////////////////////
+
+let ad = 4;
+let bd = 2;
+let resultadoDaDivisão = ad / bd;
+console.log(resultadoDaDivisão);
+///////////////////////////
+
+let ar = 8;
+let br = 3;
+let resultadoDoResto = ar % br;
+console.log(resultadoDoResto);
+///////////////////////////
+
+//Segundo Exercicio
+
+
+
+
+
+
